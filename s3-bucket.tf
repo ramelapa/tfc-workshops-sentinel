053e64b1fe39de15133c0235b8846f1a84bdc6bb
Ramesh Ellapukurthi <ramesh.elapakurthi@gmail.com>
2023-06-05 08:08:36 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,11 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "s3-bucket-for-gaurav"
+  bucket = var.prefix
   acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
-  bucket_prefix="ramesh-ellapukurthi"
 
   versioning = {
     enabled = true
